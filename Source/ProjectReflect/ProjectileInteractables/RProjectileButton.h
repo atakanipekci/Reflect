03a836a4a3d4cf3cf2bbfadc3e7ce4199d461a90
Atakan <ataipekci@gmail.com>
2023-03-26 15:29:52 +0300

    Bug fixes

--- Source/ProjectReflect/ProjectileInteractables/RProjectileButton.h
+++ Source/ProjectReflect/ProjectileInteractables/RProjectileButton.h
@@ -24,6 +24,7 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UFUNCTION()
 	void OnProjectileHit(class ARProjectile* Projectile, const FHitResult& Hit);
 
 public:	
