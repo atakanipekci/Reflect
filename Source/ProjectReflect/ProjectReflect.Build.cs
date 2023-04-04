041c50e8a8858264d70baf8908f6d81770f5a5c5
emirbeyhatun <emirbeyhatun@gmail.com>
2023-04-04 23:21:51 +0300

    Added common loading screen plugin
    *this is the plugin from Lyra Starter Game, it is made my Epic

--- Source/ProjectReflect/ProjectReflect.Build.cs
+++ Source/ProjectReflect/ProjectReflect.Build.cs
@@ -10,7 +10,7 @@ public class ProjectReflect : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayTasks"});
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "CommonLoadingScreen" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
